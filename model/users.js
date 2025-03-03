8432a6acb4462748428f911ca6691c90824f619b
yeonvity <134121395+yeonvity@users.noreply.github.com>
2025-03-03 12:39:42 +0500

    Create users.js

---
+++ model/users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// схема пользователей 
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+// модель User
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
