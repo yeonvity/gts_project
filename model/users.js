354c0676bed0ddd8c1823156905ab3062293a34d
yeonvity <134121395+yeonvity@users.noreply.github.com>
2025-03-13 11:29:20 +0500

    Update users.js

--- model/users.js
+++ model/users.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user'},
     createdAt: { type: Date, default: Date.now }
 });
 
