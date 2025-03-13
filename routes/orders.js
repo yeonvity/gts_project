32047af69a346b22bd09b319ae90d1500841d742
yeonvity <134121395+yeonvity@users.noreply.github.com>
2025-03-13 11:33:33 +0500

    Add files via upload

---
+++ routes/orders.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const Order = require('../model/orders');
+const authMiddleware = require('../middleware/auth');
+
+// оформление нового заказа
+router.post('/', authMiddleware, async (req, res) => {
+    try {
+        const { items, totalAmount } = req.body;
+        const newOrder = new Order({
+            userId: req.user.id,
+            items,
+            totalAmount
+        });
+        await newOrder.save();
+        res.json({ message: 'Заказ успешно оформлен!' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Ошибка при оформлении заказа' });
+    }
+});
+
+module.exports = router;
